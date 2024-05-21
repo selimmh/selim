da05df4e543a76f47a26f88ee6272a0062c7f8e0
selim <selim.mht@gmail.com>
2024-05-21 16:30:44 +0300

    fix: simplify meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,44 +7,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
+  title: siteConfig.name,
   description: siteConfig.description,
-  keywords: siteConfig.keywords,
-  authors: [
-    {
-      name: siteConfig.name,
-      url: siteConfig.url,
-    },
-  ],
-  creator: siteConfig.name,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/og.jpeg`],
-    creator: "@selimmh0",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
@@ -63,7 +27,7 @@ export default function RootLayout({
         <div className="flex flex-col gap-24 justify-between max-w-3xl mx-auto my-12 md:my-16 px-4 text-lg">
           <header className="">
             <a href="/" className="font-medium leading-4">
-              Selim M.
+              Selim M
             </a>
             <p className="font-medium leading-4 opacity-50">
               Software Engineer
